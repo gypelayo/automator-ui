8d6d2e754ae4ff6e258f1e5c3a89ab781f60a47a
Guilherme Pelayo <guilherme@Ernesto.lan>
2026-03-31 17:51:47 +0100

    Add debug logging to trace app loading

--- packages/automator-ui/src/main.tsx
+++ packages/automator-ui/src/main.tsx
@@ -4,7 +4,9 @@ import './index.css'
 import App from './App.tsx'
 import { initTheme } from './store/theme'
 
+console.log('main.tsx running')
 initTheme()
+console.log('theme initialized')
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
