fdd7c03c9a1ab56721361bead882f1282f1a5bed
Guilherme Pelayo <gypelayo@gmail.com>
2026-04-02 16:30:33 +0100

    Remove Infra Monitor built-in template

--- packages/automator-ui/src/templates/index.ts
+++ packages/automator-ui/src/templates/index.ts
@@ -1,10 +1,6 @@
 import { registerTemplate } from '@automator/core'
 import { travelTemplate } from './travel'
-import { infraMonitorTemplate } from './infra-monitor'
 
 export function registerAllTemplates() {
-  console.log('[templates] registerAllTemplates called')
   registerTemplate(travelTemplate)
-  registerTemplate(infraMonitorTemplate)
-  console.log('[templates] registered travel and infra-monitor')
 }
